765b771486f4bda397610e0d0120b44a593a9575
hemanth <hemanthtadikonda29@gmail.com>
2025-01-18 08:28:30 +0530

    User-Info-app  Iac_code

--- locals.tf
+++ locals.tf
@@ -1,3 +1,6 @@
 #
 
-public_subnet_ids = [ for k,v in lookup(lookup(module.subnets,"public",null),"subnets_ids",null ): v.id ]
+locals {
+  public_subnet_ids = [ for k,v in lookup(lookup(module.subnets,"public",null),"subnets_ids",null ): v.id ]
+}
+
